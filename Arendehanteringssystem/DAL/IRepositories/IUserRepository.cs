d227fba3d24a6bf3b58b3e4a4cabdcf6431b3334
asahellgren <asa@hellosugar.se>
2016-01-12 14:41:57 +0100

    Entities/IRepositories/Repositories

---
+++ Arendehanteringssystem/DAL/IRepositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.IRepositories
+{
+    public interface IUserRepository
+    {
+        List<User> GetAll();
+        User Find(int id);
+        User Add(User user);
+        User Update(User user);
+        void Remove(int id);
+    }
+}
---
+++ Arendehanteringssystem/DAL/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+using DAL.IRepositories;
+
+namespace DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
