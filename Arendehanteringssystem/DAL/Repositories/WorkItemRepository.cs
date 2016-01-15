446146bab7722e0f3c24b4b4983251909c5ec581
Mattiassvedh <mattiassvedh@gmail.com>
2016-01-15 11:14:38 +0100

    WorkItemRepo added

---
+++ Arendehanteringssystem/DAL/Repositories/WorkItemRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+using DAL.IRepositories;
+
+namespace DAL.Repositories
+{
+    public class WorkItemRepository : IWorkItem
+    {
+        public WorkItem Add(WorkItem workItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WorkItem Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WorkItem> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WorkItem Update(WorkItem workItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
