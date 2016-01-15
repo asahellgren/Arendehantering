875438ba54be096e5f481f5b98a751902d288a97
asahellgren <asa@hellosugar.se>
2016-01-15 14:16:49 +0100

    user repo with crud

--- Arendehanteringssystem/DAL/Repositories/UserRepository.cs
+++ Arendehanteringssystem/DAL/Repositories/UserRepository.cs
@@ -23,27 +23,45 @@ namespace DAL.Repositories
 
         public User Find(int id)
         {
-            return _con.Query<User>("SELECT * FROM User WHERE Id = @Id", new {id}).SingleOrDefault();
+            return _con.Query<User>("SELECT * FROM User WHERE Id = @Id").SingleOrDefault();
         }
 
         public User Add(User user)
         {
-            throw new NotImplementedException();
+            var sqlQuery = "INSERT INTO User (FirstName, LastName, UserName) VALUES(@FirstName, @LastName, @UserName)"  + "SELECT CAST(SCOPE_IDENTITY() as int";
+            var Id = _con.Query<int>(sqlQuery, user).Single();
+            user.Id = Id;
+            return user;
         }
 
         public User Update(User user)
         {
-            throw new NotImplementedException();
+            var sqlQuery =
+             "UPDATE User " +
+             "SET FirstName = @FirstName, LastName = @LastName, UserName = @UserName WHERE Id = @Id";
+            _con.Execute(sqlQuery, user);
+            return user;
         }
 
         public void Remove(int id)
         {
-            throw new NotImplementedException();
+            var sqlQuery = "DELETE FROM User WHERE Id = @Id";
+            _con.Execute(sqlQuery, id);
         }
 
-        public List<Team> GetUserWithTeams(int id)
+        public User GetUserWithTeams(int id)
         {
-            throw new NotImplementedException();
+            var sqlQueryUser = "SELECT * FROM User WHERE id = @id");
+            var sqlQueryTeam = "SELECT * FROM Team T INNER JOIN UserTeam UT ON T.Id = UT.TeamId WHERE UT.UserId=@Id";
+            var user =_con.Query<User>(sqlQueryUser).SingleOrDefault();
+            var teams =_con.Query<Team>(sqlQueryTeam).ToList();
+
+            user.Teams = teams;
+            return user;
+
+
+
+
         }
     }
 }
