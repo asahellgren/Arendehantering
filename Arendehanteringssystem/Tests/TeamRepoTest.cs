aadba0f8045714c3649a60f37b25b508efea2850
Tobias Balzano <contact@tobiasbalzano.se>
2016-01-15 15:53:33 +0100

    added tests for TeamRepo and committing changes

---
+++ Arendehanteringssystem/Tests/TeamRepoTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DAL.Repositories;
+using DAL.Entities;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TeamRepoTest
+    {
+        [OneTimeSetUp]
+        public void Seed()
+        {
+            var dbContext = new TeamRepository();
+            dbContext.Add(new Team { Name = "Team1" });
+            dbContext.Add(new Team { Name = "Team2" });
+            dbContext.Add(new Team { Name = "Team3" });
+            dbContext.Add(new Team { Name = "Team4" });
+        }
+
+        [Test]
+        public void GetAllTeams()
+        {
+            var dbContext = new TeamRepository();
+            var result = dbContext.GetAll();
+            Assert.NotNull(result);
+        }
+    }
+}
