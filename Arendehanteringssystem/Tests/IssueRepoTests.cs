6cd8414be45299381afa456dfda238bf25ea05d9
JensBackelin <jensbackelin@hotmail.com>
2016-01-18 11:36:59 +0100

    ändring i Issuetest

---
+++ Arendehanteringssystem/Tests/IssueRepoTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+using DAL.Repositories;
+using NUnit.Framework;
+
+
+namespace Tests
+{
+    [TestFixture]
+    public class IssueRepoTests
+    {
+        [OneTimeSetUp]
+        public void TempDb()
+        {
+            
+        }
+    }
+}
