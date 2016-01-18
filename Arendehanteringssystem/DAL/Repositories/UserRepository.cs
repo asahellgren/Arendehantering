9c8b5b115e810570c5afa61f81e3b38e1f07c539
asahellgren <asa@hellosugar.se>
2016-01-18 11:27:18 +0100

    UserRepo and noncompleted tests

--- Arendehanteringssystem/DAL/Repositories/UserRepository.cs
+++ Arendehanteringssystem/DAL/Repositories/UserRepository.cs
@@ -14,30 +14,30 @@ namespace DAL.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        private IDbConnection _con = new SqlConnection(ConfigurationManager.ConnectionStrings["Arendehantering"].ConnectionString);
+        private readonly IDbConnection _con = new SqlConnection(ConfigurationManager.ConnectionStrings["Arendehantering"].ConnectionString);
 
         public List<User> GetAll()
         {
-            return _con.Query<User>("SELECT * FROM User").ToList();
+            return _con.Query<User>("SELECT * FROM [User]").ToList();
         }
 
         public User Find(int id)
         {
-            return _con.Query<User>("SELECT * FROM User WHERE Id = @Id").SingleOrDefault();
+            return _con.Query<User>("SELECT * FROM [User] WHERE Id = @Id").SingleOrDefault();
         }
 
         public User Add(User user)
         {
-            var sqlQuery = "INSERT INTO User (FirstName, LastName, UserName) VALUES(@FirstName, @LastName, @UserName)"  + "SELECT CAST(SCOPE_IDENTITY() as int";
-            var Id = _con.Query<int>(sqlQuery, user).Single();
-            user.Id = Id;
+            var sqlQuery = "INSERT INTO [User] (FirstName, LastName, UserName) VALUES(@FirstName, @LastName, @UserName)"  + "SELECT Id FROM [User] WHERE Id = SCOPE_IDENTITY()";
+            var userId = _con.Query(sqlQuery, new {user.FirstName, user.LastName, user.UserName} ).First();
+            user.Id = userId.Id;
             return user;
         }
 
         public User Update(User user)
         {
             var sqlQuery =
-             "UPDATE User " +
+             "UPDATE [User] " +
              "SET FirstName = @FirstName, LastName = @LastName, UserName = @UserName WHERE Id = @Id";
             _con.Execute(sqlQuery, user);
             return user;
@@ -45,13 +45,13 @@ namespace DAL.Repositories
 
         public void Remove(int id)
         {
-            var sqlQuery = "DELETE FROM User WHERE Id = @Id";
+            var sqlQuery = "DELETE FROM [User] WHERE Id = @Id";
             _con.Execute(sqlQuery, id);
         }
 
         public User GetUserWithTeams(int id)
         {
-            var sqlQueryUser = "SELECT * FROM User WHERE id = @id";
+            var sqlQueryUser = "SELECT * FROM [User] WHERE id = @id";
             var sqlQueryTeam = "SELECT * FROM Team T INNER JOIN UserTeam UT ON T.Id = UT.TeamId WHERE UT.UserId=@Id";
             var user =_con.Query<User>(sqlQueryUser).SingleOrDefault();
             var teams =_con.Query<Team>(sqlQueryTeam).ToList();
--- Arendehanteringssystem/Tests/UserRepoTests.cs
+++ Arendehanteringssystem/Tests/UserRepoTests.cs
@@ -7,17 +7,58 @@ using DAL.Entities;
 using DAL.Repositories;
 using Microsoft.Win32;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace Tests
 {
     [TestFixture]
     public class UserRepoTests
     {
+
+        [OneTimeSetUp]
+        public void Seed()
+        {
+            _dbContext.Add(new User {FirstName = "Åsa", LastName = "Skoog", UserName = "skoog"});
+            _dbContext.Add(new User { FirstName = "Jens", LastName = "Backelin", UserName = "backelin" });
+            _dbContext.Add(new User { FirstName = "Tobias", LastName = "Balzano", UserName = "balzano" });
+        }
+
+        readonly UserRepository _dbContext = new UserRepository();
         [Test]
         public void GetAllUsers()
-        {
-            var dbContext = new UserRepository();
-            var users = dbContext.GetAll();
+        {         
+            var users = _dbContext.GetAll();
+            Assert.That(users != null);
         }
+
+        public void Find()
+        {
+            
+        }
+
+        [Test]
+        public void InsertUser()
+        {
+            var user = new User {FirstName = "Jens", LastName = "Backelin", UserName = "jensbackelin"};
+            var addedUser = _dbContext.Add(user);
+            Assert.That(addedUser != null);
+        }
+
+        [Test]
+        public void UpdateUser()
+        {
+            var user = new User { Id =1, FirstName = "Åsa", LastName = "Skoog", UserName = "skoooooog" };
+            var updatedUser =_dbContext.Update(user);
+            Assert.That(updatedUser != null);
+        }
+
+        //[Test]
+        //public void DeleteUser()
+        //{
+
+        //    var id = 1;
+        //    _dbContext.Remove(id);
+        //    Assert.That( != null);
+        //}
     }
 }
